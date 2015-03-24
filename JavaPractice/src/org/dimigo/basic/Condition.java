f95c38ce1e6c4f15875f9353281fec476c584a5e
user' <user'@user-PC>
2015-03-24 15:06:05 +0900

    수정본

--- JavaPractice/src/org/dimigo/basic/Condition.java
+++ JavaPractice/src/org/dimigo/basic/Condition.java
@@ -13,29 +13,29 @@ public class Condition {
 		
 		switch(trans){
 		case "고속버스" :
-			if(dis==10){
+			if(dis<=10){
 				fee = 850;
-			}else if (dis <20){
+			}else if (dis <=20){
 				fee = 1150;
-			}else if(dis < 30){
+			}else if(dis <= 30){
 				fee = 1450;
 			}else {
 				fee = 1450 + (dis/10)*300;
 			}
 			break;
 		case "경차" :
-			if(dis==10){
+			if(dis<=10){
 				fee = 300;
-			}else if (dis <20){
+			}else if (dis <= 20){
 				fee = 500;
-			}else if(dis < 30){
+			}else if(dis <= 30){
 				fee = 700;
 			}else {
 				fee = 700 + (dis/10)*200;
 			}
 			break;
 		case "그 외" :
-			if(dis==10){
+			if(dis<=10){
 				fee = 600;
 			}else {
 				fee = 600 + (dis/10)*200;
