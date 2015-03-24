8a04455e0d236ec0f35954498a0c4cc0dbeef0ac
user' <user'@user-PC>
2015-03-24 15:16:03 +0900

    최종 수정본

--- JavaPractice/src/org/dimigo/basic/Condition.java
+++ JavaPractice/src/org/dimigo/basic/Condition.java
@@ -7,29 +7,20 @@ public class Condition {
 		
 		System.out.println("<< 고속도로 통행료 계산 >>");
 		
-		String trans="그 외";
-		int fee = 0;
-		int dis = 25;
+		String trans="고속버스";
+		int fee = 0, dis = 10;
 		
 		switch(trans){
 		case "고속버스" :
 			if(dis<=10){
 				fee = 850;
-			}else if (dis <=20){
-				fee = 1150;
-			}else if(dis <= 30){
-				fee = 1450;
 			}else {
-				fee = 1450 + (dis/10)*300;
+				fee = 850 + (dis/10)*300;
 			}
 			break;
 		case "경차" :
 			if(dis<=10){
 				fee = 300;
-			}else if (dis <= 20){
-				fee = 500;
-			}else if(dis <= 30){
-				fee = 700;
 			}else {
 				fee = 700 + (dis/10)*200;
 			}
