1bc9b81803e3721d038583f9a7750917c33e527f
user' <user'@user-PC>
2015-09-24 14:25:59 +0900

    ㅁㅁ

---
+++ JavaPractice/src/org/dimigo/collection/MelonChart.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package org.dimigo.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <pre>
+ * org.dimigo.basic
+ *	 |_ org.dimigo.collection
+
+ * 1: 개요 : 
+ * 2: 작성일 : 2015. 9. 24.
+ * </pre>
+ *
+ * @author : 
+ * @version : 
+ */
+public class MelonChart {
+	public static void main(String[] args){
+		ArrayList<Music> list = new ArrayList<Music>();
+		
+		
+		list.add(new Music("바람이나 좀 쐐", "개리"));
+		list.add(new Music("보통연애", "박경"));
+		list.add(new Music("취향저격", "iKON"));
+		
+		System.out.println("--<<멜론차트>>--");
+		printList(list);
+		
+		list.add(1, new Music("레옹", "이유갓지"));
+		System.out.println("\n--<<2위 곡 추가>>--");
+		printList(list);
+		
+		list.set(2, new Music("맙소사", "황태지"));
+		System.out.println("\n--<<3위 곡 변경>>--");
+		printList(list);
+		
+		list.remove(3);
+		System.out.println("\n--<<4위 곡 삭제>>--");
+		printList(list);
+		
+		list.clear();
+		System.out.println("\n--<<전체 리스트 삭제>>--");
+		printList(list);		
+		
+	}
+	
+	public static void printList(List<Music> list){
+		for(Music value: list){
+			System.out.println(value);
+		}		
+	}
+}
---
+++ JavaPractice/src/org/dimigo/collection/Music.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package org.dimigo.collection;
+
+/**
+ * <pre>
+ * org.dimigo.basic
+ *	 |_ org.dimigo.collection
+
+ * 1: 개요 : 
+ * 2: 작성일 : 2015. 9. 23.
+ * </pre>
+ *
+ * @author : 
+ * @version : 
+ */
+public class Music {
+	private String title;
+	private String singer;
+	
+	public Music(String title, String singer){
+		this.title = title;
+		this.singer = singer;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getSinger() {
+		return singer;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setSinger(String singer) {
+		this.singer = singer;
+	}
+
+	
+	
+	@Override
+	public String toString() {
+		return "Music [title=" + title + ", singer=" + singer + "]";
+	}
+	
+}
